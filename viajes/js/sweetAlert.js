ed3690481d41a0b4e32e684598117d4c127f8ae4
fernando-schabrich <114669702+fernando-schabrich@users.noreply.github.com>
2023-02-09 20:54:41 -0300

    Add files via upload

---
+++ viajes/js/sweetAlert.js
@@ -0,0 +1,11 @@
+Swal.fire({
+  title: '¡Bienvenidos a Japón!',
+  text: 'Con nosotros va a encontrar los mejores precios del mercado.',
+  
+  width: 600,
+  padding: '3em',
+  color: '#9F342D',
+  background: '#AABDD1 url(./img/plane.gif) no-repeat center ',
+  
+  
+})
